3548944ef22c2ce7eb8e9bedd673e420302c1583
andron3a <andron3a@mail.ru>
2022-12-02 00:36:41 +0300

    Номер сотрудника должен быть числовой

--- test_module_employee.py
+++ test_module_employee.py
@@ -5,3 +5,8 @@ from module_employee import Employee, Department
 def test_wrong_salary():
     with pytest.raises(ValueError):
         employee_1 = Employee(1, "Семенов М.А.", "12.12.2000", -100)
+
+# Номер сотрудника должен быть числовой
+def test_wrong_type_Employee():
+    with pytest.raises(TypeError):
+        employee_1 = Employee("1", "Семенов М.А.", "12.12.2000", 10000)
