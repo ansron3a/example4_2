d0da06724600d4df23f40d4553349233891d297e
andron3a <andron3a@mail.ru>
2022-12-02 00:26:26 +0300

    Тестовые функции

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pytest
 
-#тестируемая функция
+
+# тестируемая функция
 def arifmeticle(a, b, op):
     if op == '+':
         return a + b
@@ -10,5 +11,27 @@ def arifmeticle(a, b, op):
         return a * b
     elif op == '/':
         return a / b
+    raise ValueError(f"Неисвестный оператор: {op}")
 
-print (arifmeticle(4,5,'*'))
+
+def test_unknow_op():
+    with pytest.raises(ValueError):
+        print(arifmeticle(2, 3, '&'))
+
+
+def test_not_number_val():
+    with pytest.raises(TypeError):
+        print(arifmeticle(5, 3, '+'))
+
+
+def test_dew_zero():
+    with pytest.raises(ZeroDivisionError):
+        print(arifmeticle(3, 0, '/'))
+
+
+def test_arifm_1():
+    assert arifmeticle(3, 4, '*') == 12
+
+
+def test_arifm_2():
+    assert arifmeticle(3, 4, '-') == -1
